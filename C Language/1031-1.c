8752544bcd705b21c479d8b8fc904face98d4469
issiki9401 <nvst9680@gmail.com>
2025-11-03 17:42:52 +0800

    new file:   C Language/1031-1.c 	new file:   C Language/skill.txt

---
+++ C Language/1031-1.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    unsigned int m;
+    
+    printf("请输入一个非负整数 m: ");
+    scanf("%u", &m);
+    
+    // 获取千位数和百位数
+    int thousand_digit = (m / 1000) % 10;
+    int hundred_digit = (m / 100) % 10;
+    
+    // 计算交换后的数值
+    int swapped_m = m;
+    
+    // 减去原来的千位数和百位数
+    swapped_m -= thousand_digit * 1000;
+    swapped_m -= hundred_digit * 100;
+    
+    // 加上交换后的千位数和百位数
+    swapped_m += hundred_digit * 1000;
+    swapped_m += thousand_digit * 100;
+    
+    printf("原始数值: %u\n", m);
+    printf("交换后的数值: %d\n", swapped_m);
+    
+    return 0;
+}
