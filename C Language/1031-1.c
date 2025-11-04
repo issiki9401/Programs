9d1cd2ceedb21f9afc1e88ecd2ccd168e83b611c
issiki9401 <nvst9680@gmail.com>
2025-11-04 18:14:14 +0800

    modified:   C Language/1031-1.c

--- C Language/1031-1.c
+++ C Language/1031-1.c
@@ -1,27 +1,16 @@
-
 #include <stdio.h>
-#include <math.h>
 
 int main() {
     unsigned int m;
     
-    printf("請輸入一個非負整數 m: ");
     scanf("%u", &m);
     
     int thousand_digit = (m / 1000) % 10;
     int hundred_digit = (m / 100) % 10;
     
-    int swapped_m = m;
-    
-    swapped_m -= thousand_digit * 1000;
-    swapped_m -= hundred_digit * 100;
-    
-    swapped_m += hundred_digit * 1000;
-    swapped_m += thousand_digit * 100;
-    
-    printf("原始數值: %u\n", m);
-    printf("交換後的數值: %d\n", swapped_m);
+    m = m - (thousand_digit * 1000) - (hundred_digit * 100) + (hundred_digit * 1000) + (thousand_digit * 100);
+ 
+    printf("%d\n", m);
     
     return 0;
 }
-//小記：不一定要創立spapped_m，可以直接使用m計算（只是為了清晰表示交換前後的差異）
