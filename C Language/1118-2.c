2a8df869ac992ad59444b392c1147523af493d37
issiki9401 <nvst9680@gmail.com>
2025-12-09 15:32:57 +0800

    modified:   C Language/1118-2.c

--- C Language/1118-2.c
+++ C Language/1118-2.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 
-int compare_diff(int m1, int m2) {
-    return m1 - m2;  // 直接返回差值
-}
-
-int main() {
-    int a = 0, b = 0;
-    printf("請輸入兩個整數");
+int main(){
+    int a, b;
     scanf("%d %d", &a, &b);
-    
-    int res = compare_diff(a, b);
-    printf("差值結果：%d\n", res);
+    a-=b;
+
+    printf("%d\n", a);
     return 0;
 }
