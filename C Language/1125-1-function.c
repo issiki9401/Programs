b8975d928687d395ef58374c760784f94aff62da
issiki9401 <nvst9680@gmail.com>
2025-12-15 18:37:07 +0800

    modified:   C Language/1125-1-function.c

--- C Language/1125-1-function.c
+++ C Language/1125-1-function.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 
-void calculate(int m, int count) {
-        while(m!=0){
-            m/=10;
-            if(m%2==0){
-                count++;
-            }
+void calculate(int);
+
+void calculate(int m) {
+    int count=0;
+    while(m!=0){
+        m/=10;
+        if(m%2==0){
+            count++;
         }
-        printf("%d", count);
+    }
+    printf("%d", count);
 }
 
 int main() {
-    int m, count=0;
+    int m;
     scanf("%d", &m);
     
-    calculate(m, count);
+    calculate(m);
     printf("\n");
     return 0;
 }
