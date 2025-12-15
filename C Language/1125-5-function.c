1b7ee32a43f2fb5adcc50988d3f509260883be3c
Issiki9401 <155891431+issiki9401@users.noreply.github.com>
2025-12-15 17:12:29 +0800

    创建 1125-5-function.c

---
+++ C Language/1125-5-function.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int a(int, int);
+
+int a(int a, int b)
+{
+    int quotient = 0;
+
+    while(m>=n){
+        m-=n;
+        quotient++;
+
+    return quotient;
+}
+
+int main(){
+    int m, n = 0;
+    scanf("%d %d",&m,&n);
+					
+    ans = a(m, n);
+
+    printf("%d\n", ans);
+
+    return 0;
+}
