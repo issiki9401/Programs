05af35259cab5f1a17681993c61780ff60defde8
issiki9401 <nvst9680@gmail.com>
2025-12-23 17:11:54 +0800

    new file:   C Language/1222-6-test.c

---
+++ C Language/1222-6-test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int a(int, int);
+
+int a(int m, int i){
+    if(i==m)
+        return 1;
+    if(m==i*i)
+        return 2;
+    return a(m, i+1);
+}
+
+int main(){
+    int m, i=1;
+    scanf("%d",&m);
+
+if(a(m, i)==2)
+    printf("yes\n");
+else
+    printf("no\n");
+    
+    return 0;
+}
