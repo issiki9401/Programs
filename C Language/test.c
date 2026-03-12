ca3f898191adfbd39cf04236519a457634579afd
issiki9401 <nvst9680@gmail.com>
2026-03-12 11:41:21 +0800

    modified:   C Language/test.c

--- C Language/test.c
+++ C Language/test.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-    int m, n=0;
-    printf("%d %d", m, n);
-    
+    int n;
+    int total_sum = 0;
+
+    if (scanf("%d", &n) != 1 || n <= 0){
+        return 1;
+    }
+
+    int arr[n];
+
+    for(int i = 0; i <n; i++){
+        scanf("%d", &arr[i]);
+    }
+
     return 0;
 }
