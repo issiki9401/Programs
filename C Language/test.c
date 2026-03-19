4c3d482ecd6b7e6c87a44fd088bde8f9e5277f80
issiki9401 <nvst9680@gmail.com>
2026-03-19 11:53:42 +0800

    modified:   C Language/test.c

--- C Language/test.c
+++ C Language/test.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    int n;
+int sumOfAllDigits(int n) {
     int total_sum = 0;
-
-    if (scanf("%d", &n) <= 0){
-        return 1;
     }
+    return n;
+}
+
+int main(){
+    int n, arr[100], tatal_sum;
+    scanf("%d", &n);
+    for(int i=0; i<=n; i++){
+        scanf("%d", &arr[i]);
+    }
+    printf("1. 所有位數的和: %d\n", sumOfAllDigits(n));
 
     return 0;
 }
