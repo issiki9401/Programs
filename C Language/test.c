c4baab045b55424f9f8dcf02d42af7fa5db65556
Issiki9401 <155891431+issiki9401@users.noreply.github.com>
2026-04-08 23:57:04 +0800

    更新 test.c

--- C Language/test.c
+++ C Language/test.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+
+char* findlastCity(char *cities[], int size){
+    if(size <= 0){
+        return NULL; 
+    }
+    char *lastCity = cities[0];
+    for(int i = 1; i < size; i++){
+        if(strcmp(cities[i], lastCity) > 0){
+            lastCity = cities[i];
+        }
+    }
+    return lastCity;
+}
 
 int main(){
-    printf("Hello, World!\n");
-    
+    char *cities[ ] = {"London", "New York", "Tokyo", "Paris", "Taipei"};
+    int size = sizeof(cities) / sizeof(cities[0]);
+    char *result = findlastCity(cities, size);
+    if(result != NULL){
+        printf(result);
+    }
+    else{
+        printf("Can't find the city\n");
+    }
+
     return 0;
 }
